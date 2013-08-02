00048c41bc65dfd180ccce7e542e42d7a81bd63f
Patrick Slattery <slattery.patrickj@gmail.com>
2013-08-02 15:13:43 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Happ::Application.config.secret_token = 'fa6e9e8356cac0e522710500c9175e2d8ccb8f9b93872aaf3d4bf72b91743af26c8ab3b1fe46eaa2da57449951db861bdabcf2e08a39c7963b7fc4515405cc15'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Happ::Application.config.session_store :cookie_store, key: '_happ_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Happ::Application.config.session_store :active_record_store
