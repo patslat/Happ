29e45d51c61edf7c7c41c7077e2049dc0ebf67c6
Patrick Slattery <slattery.patrickj@gmail.com>
2013-08-03 18:16:21 -0700

    switch backbone to javascript

---
+++ app/assets/javascripts/happ.js
@@ -0,0 +1,13 @@
+window.Happ = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Happ.initialize();
+});
