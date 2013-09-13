d84c24ae1d2bc2671b19db0e1e3f186cd8989883
Patrick Slattery <slattery.patrickj@gmail.com>
2013-09-13 16:36:17 -0700

    update seed for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,15 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) can be set in the file config/application.yml.
 # See http://railsapps.github.io/rails-environment-variables.html
-puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-puts 'user: ' << user.name
+#puts 'DEFAULT USERS'
+#user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+#puts 'user: ' << user.name
 
 u = User.create(:name => "test", :email => "test@test.com", :password => "12345678", :password_confirmation => "12345678")
 
 today = DateTime.now
 ((today - 1000)..today).each_with_index do |day, idx|
-  day_model = Day.create(:user_id => 2)
+  day_model = Day.create(:user_id => 1)
   day_model.date = day.to_date.readable_inspect
   day_model.save!
 
