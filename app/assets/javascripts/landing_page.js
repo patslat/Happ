102660077259be3dcf450c18f106b6c4f121ae78
Patrick Slattery <slattery.patrickj@gmail.com>
2013-09-16 11:53:09 -0700

    add demo tooltip and better seed

---
+++ app/assets/javascripts/landing_page.js
@@ -0,0 +1,7 @@
+$(function () {
+  $('.login').tooltip({
+    placement: 'bottom',
+    title: 'Demo Account: test@test.com\nPassword: 12345678',
+    trigger: 'manual'
+  }).tooltip('show');
+});
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,18 +14,29 @@
 u = User.create(:name => "test", :email => "test@test.com", :password => "12345678", :password_confirmation => "12345678")
 
 today = DateTime.now
-((today - 1000)..today).each_with_index do |day, idx|
+
+ratings = [4.0, 3.8, 3.0, 3.5, 2.8]
+
+
+((today - 100)..today).each_with_index do |day, idx|
+  ratings = ratings.map do |rating|
+    r = rating + (rating / 100.0)
+    if r > 9.8
+      r = 9.8
+    end
+    r
+  end
   day_model = Day.create(:user_id => 1)
   day_model.date = day.to_date.readable_inspect
   day_model.save!
 
   Rating.create(
     :day_id => day_model.id,
-    :overall_rating => (10 - rand),
-    :sleep_rating => (7 - rand),
-    :diet_rating => (6 - rand),
-    :exercise_rating => (9 - rand),
-    :social_rating => (8 - rand)
+    :overall_rating => ratings[0],
+    :sleep_rating => ratings[1],
+    :diet_rating => ratings[2],
+    :exercise_rating => ratings[3],
+    :social_rating => ratings[4]
   )
 
 end
